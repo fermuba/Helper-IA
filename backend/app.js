b002aa66585af583ea4e233debf0af7d5b103f53
Daniela Homobono <110701158+danielaHomobono@users.noreply.github.com>
2025-11-25 14:43:05 -0300

    merge: resuelvo conflicto en app.js, mantengo config CORS completa

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,4 @@
 const express = require('express');
-<<<<<<< HEAD
-const cors = require('cors');   // ⬅️ agregar esto
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-// ⬅️ habilitar CORS antes del router
-app.use(cors({
-  origin: 'http://localhost:5175',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type']
-=======
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,7 +7,6 @@ const port = process.env.PORT || 3000;
 app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
   credentials: true
->>>>>>> 7793451690122b27ed8205e6dcbbdd1ed3dff3f6
 }));
 
 // Para poder recibir JSON desde el frontend
